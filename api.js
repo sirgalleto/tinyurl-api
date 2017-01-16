0f48c756eb3f8b3aad7c6cd8c46d19f016f33e03
sirgalleto <sebastian.osorio@wizeline.com>
2017-01-16 16:41:05 -0600

    (update) remove logs

--- api.js
+++ api.js
@@ -6,7 +6,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  console.log('yeah');
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
