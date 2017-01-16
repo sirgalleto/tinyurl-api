929d95d49d8fd4e04cb81f681ee5bb8b2a5e02ac
sirgalleto <sebastian.osorio@wizeline.com>
2017-01-16 16:37:56 -0600

    (update) allow cors

--- api.js
+++ api.js
@@ -5,6 +5,13 @@ let express     = require('express')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  console.log('yeah');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 function Api(port, routes) {
   this._router = express.Router();
 
@@ -21,7 +28,7 @@ function Api(port, routes) {
       reject(e);
     }
 
-    resolve(app);    
+    resolve(app);
   });
 
   this._registerRoutes = () => {
