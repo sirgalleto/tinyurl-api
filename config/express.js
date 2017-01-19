6dad984e0a93f3744e9fcbe9a3766b58fee314e3
sirgalleto <sebastian.osorio@wizeline.com>
2017-01-19 13:12:34 -0600

    (update) express configs

---
+++ config/express.js
@@ -0,0 +1,16 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+export default function(app) {
+  app.set('case sensitive routing', true);
+  app.use(cors());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  app.use(function(req, res, next) {
+    res.header('Content-Type','application/json');
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    next();
+  });
+}
