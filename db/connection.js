5c93f0849c772ea4bb05bf5888c17d02a5dd82df
sirgalleto <sebastian.osorio@wizeline.com>
2017-01-19 13:07:25 -0600

    (update) db refactor

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Dependencies
+
+import mongoose from 'mongoose';
+
+// Mongo connection
+
+module.exports = function({mongoDB}) {
+  mongoose.connect(mongoDB.uri, mongoDB.options);
+  mongoose.connection.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+    process.exit(-1);
+  });
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+// Main file
+
+'use strict';
+
+const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Dependencies
+import mongoose from 'mongoose';
+
+import createConfig from './config/environment';
+import connection from './db/connection';
+import Api from './api';
+import urlRoutes from './app/routes/url';
+
+const config = createConfig(env);
+connection(config);
+
+const port = config.port;
+
+const routes = [
+  ...urlRoutes
+];
+
+let api = new Api(port, routes);
+let app = api.start();
+
+module.exports = app;
