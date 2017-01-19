3cb546a554da30a1bac8e7b168c5a5b94b09f7a4
sirgalleto <sebastian.osorio@wizeline.com>
2017-01-19 10:40:37 -0600

    (update) env configurations

---
+++ config/env/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Dependencies
+
+import _ from 'lodash';
+
+// All configurations will extend these base options
+
+let base = {
+  env: process.env.NODE_ENV,
+  port: process.env.port || 8080,
+  mongoDB: {
+    options: {
+      db: {
+        safe: true
+      }
+    }
+  }
+};
+
+// Export the config object based on the NODE_ENV
+
+module.exports = function(env) {
+  console.info(env);
+  return _.merge(base, require('./' + env + 'env.js') || {});
+};
